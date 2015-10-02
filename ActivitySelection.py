9fdc19532c600a7a1701a5ddcf25f7daff8fef57
Shen <yiransh@amazon.com>
2015-10-02 00:17:18 -0700

    Fix Activity Selection

--- ActivitySelection.py
+++ ActivitySelection.py
@@ -1,11 +1,21 @@
 # http://www.geeksforgeeks.org/greedy-algorithms-set-1-activity-selection-problem/
 class Solution:
     def selectActivity(self, activities):
-
+        activities.sort(key=lambda x: x[1])
+        result = []
+        result.append(activities[0])
+        prev_selected = activities[0]
+        for i in range(1, len(activities)):
+            if prev_selected[1] <= activities[i][0]:
+                result.append(activities[i])
+                prev_selected = activities[i]
+        return result
 
 if __name__ == "__main__":
     # [(start_time, end_time), ...]
     activities = [(1,2), (3,4), (8,9), (0,6), (5,7), (5,9)]
     # output: activities selected
     sol = Solution()
-    sol.selectActivity(activities)
+    print sol.selectActivity(activities)
+    # output: right answer
+    print [(1,2), (3,4), (5,7), (8,9)]
