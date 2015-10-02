8da90079bae39329c9f82137bae9c58fb352b624
Shen <yiransh@amazon.com>
2015-10-01 23:42:42 -0700

    first commit - activity selection geeksforgeeks

---
+++ ActivitySelection.py
@@ -0,0 +1,11 @@
+# http://www.geeksforgeeks.org/greedy-algorithms-set-1-activity-selection-problem/
+class Solution:
+    def selectActivity(self, activities):
+
+
+if __name__ == "__main__":
+    # [(start_time, end_time), ...]
+    activities = [(1,2), (3,4), (8,9), (0,6), (5,7), (5,9)]
+    # output: activities selected
+    sol = Solution()
+    sol.selectActivity(activities)
